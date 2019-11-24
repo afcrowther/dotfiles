633769d31b4cd38b01881b944bfe7d49af0e75e4
afcrowther <afcrowther@icloud.com>
2019-11-24 10:33:19 +0000

    Add bash aliases

--- setup.sh
+++ setup.sh
@@ -5,6 +5,15 @@ if [ "~/.bashrc" ]; then
 	rm ~/.bashrc
 fi
 
+# remove original aliases
+if [ "~/.aliases" ]; then
+	rm ~/.aliases
+fi
+
 # change dir to correct location
 cd "$(dirname "$0")"
 ln -s "$(pwd)/bashrc" ~/.bashrc
+ln -s "$(pwd)/aliases" ~/.aliases
+
+# load changes into shell
+. ~/.bashrc
