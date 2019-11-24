e0dbd352291becc9e68cd6d81fe9fb1e8a203ce9
Adam Crowther <afcrowther@icloud.com>
2019-11-24 10:47:22 +0000

    Add git config

--- setup.sh
+++ setup.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 
 # remove original bashrc
-if [ "~/.bashrc" ]; then
+if [ -f ~/.bashrc ]; then
 	rm ~/.bashrc
 fi
 
 # remove original aliases
-if [ "~/.aliases" ]; then
+if [ -f ~/.aliases ]; then
 	rm ~/.aliases
+fi
+
+# remove original git config
+if [ -f ~/.gitconfig ]; then
+	rm ~/.gitconfig
 fi
 
 # change dir to correct location
 cd "$(dirname "$0")"
 ln -s "$(pwd)/bashrc" ~/.bashrc
 ln -s "$(pwd)/aliases" ~/.aliases
+ln -s "$(pwd)/gitconfig" ~/.gitconfig
 
 # load changes into shell
 . ~/.bashrc
